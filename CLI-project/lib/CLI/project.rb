d0e384fb3af887c201112b286158bcdf5dac4688
Doug Kintop <dkintop@hotmail.com>
2019-09-30 14:24:55 +0000

    modified readme

--- CLI-project/lib/CLI/project.rb
+++
@@ -1,8 +0,0 @@
-require "CLI/project/version"
-
-module CLI
-  module Project
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- CLI-project/lib/CLI/project/version.rb
+++
@@ -1,5 +0,0 @@
-module CLI
-  module Project
-    VERSION = "0.1.0"
-  end
-end
